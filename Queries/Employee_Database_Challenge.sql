6171cf7f750eaab8c43b6aa4fe9894ca1f68a144
Derek <derek.mears@outlook.com>
2022-06-18 14:48:58 -0400

    adding csv files and employee_db.sql file

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,44 @@
+--Employee_Database_Challenge
+--create table of retirement titles
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO Retirement_Titles
+FROM employees e
+	INNER JOIN titles t
+	ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+-- create table of unique titles
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles rt
+WHERE rt.to_date = '9999-01-01'
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- PH-EmployeeDB=# \COPY unique_titles TO 'c:\users\derek\documents\bootcamp\module 7\Pewlett-Hackard-Analysis\Data\uniq
+-- ue_titles.csv' with csv header
+-- COPY 72458
+
+--query to retreive # of employees about to retire, by job title
+
+-- first table of unique titles for employees about to retire
+-- creating table of unique titles
+SELECT COUNT (ut.emp_no) as "Employee_Count", ut.title
+INTO retiring_titles
+FROM unique_titles ut
+GROUP BY ut.title
+ORDER BY "Employee_Count" desc;
+
+
+-- PH-EmployeeDB=# \COPY  retiring_titles TO 'c:\users\derek\documents\bootcamp\module 7\Pewlett-Hackard-Analysis\Data\r
+-- etiring_titles.csv' with csv header
+-- COPY 7
