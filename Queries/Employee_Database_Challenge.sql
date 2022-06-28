fb654db9ee54d433c0b6995bd3009e789c31b583
Derek <derek.mears@outlook.com>
2022-06-27 23:12:41 -0400

    update mentorship_eligibility

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -48,6 +48,9 @@ ORDER BY "Employee_Count" desc;
 --write a query to create a Mentorship Eligibility table that holds the employees who are eligible
 --to participate in a mentorship program.
 
+--DROP TABLE IF EXISTS mentorship_eligibility
+--CASCADE;
+
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -62,6 +65,14 @@ ON (e.emp_no = de.emp_no)
 INNER JOIN titles t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+and (de.to_date='9999-01-01')
 ORDER BY e.emp_no;
 
+select count(*) from mentorship_eligibility;
 
+--create 
+SELECT COUNT (me2.emp_no) as "Employee_Count", me2.title
+--INTO retiring_titles
+FROM mentorship_eligibility2 me2
+GROUP BY me2.title
+ORDER BY "Employee_Count" desc;
